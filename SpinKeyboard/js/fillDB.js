4c42b52ecb0c66ddf2ad17dbb81d7c09ef48ff1c
Jackie <Jackie@JackieHP>
2016-07-14 17:33:27 +0300

    transaction error

--- SpinKeyboard/js/fillDB.js
+++ SpinKeyboard/js/fillDB.js
@@ -4,15 +4,159 @@ var indexedDB = window.indexedDB || window.mozIndexedDB
 
 (function() {
 	var wordData = [ {
-		pattern : '+19-11+17-21+9',
+		pattern : '20,9,26,5,14',
 		word : 'tizen'
 	}, {
-		pattern : '+22-18+14-14',
+		pattern : '1,23,5,19,15,13,5',
 		word : 'awesome'
 	}, {
-		pattern : '+18',
+		pattern : '9,17',
 		word : 'is'
-	} ];
+	}, {
+		pattern : '20,8,5 ',
+		word : 'the'
+	}, {
+		pattern : '2,5',
+		word : 'be'
+	}, {
+		pattern : '20,15',
+		word : 'to'
+	}, {
+		pattern : '15,6',
+		word : 'of'
+	}, {
+		pattern : '1,14,04',
+		word : 'and'
+	}, {
+		pattern : '1',
+		word : 'a'
+	}, {
+		pattern : '9,14',
+		word : 'in'
+	}, {
+		pattern : '20,8,1,20',
+		word : 'that'
+	}, {
+		pattern : '8,1,22,5',
+		word : 'have'
+	}, {
+		pattern : '9',
+		word : 'I'
+	}, {
+		pattern : '9,20',
+		word : 'it'
+	}, {
+		pattern : '6,15,18',
+		word : 'for'
+	}, {
+		pattern : '14,15,20',
+		word : 'not'
+	}, {
+		pattern : '15,14',
+		word : 'on'
+	}, {
+		pattern : '23,9,20,8',
+		word : 'with'
+	}, {
+		pattern : '8,5',
+		word : 'he'
+	}, {
+		pattern : '1,19',
+		word : 'as'
+	}, {
+		pattern : '25,15,21',
+		word : 'you'
+	}, {
+		pattern : '1,20',
+		word : 'at'
+	}, {
+		pattern : '20,8,9,19',
+		word : 'this'
+	}, {
+		pattern : '2,21,20',
+		word : 'but'
+	}, {
+		pattern : '8,9,19',
+		word : 'his'
+	}, {
+		pattern : '2,25',
+		word : 'by'
+	}, {
+		pattern : '6,18,15,13',
+		word : 'from'
+	}, {
+		pattern : '20,8,5,25',
+		word : 'they'
+	}, {
+		pattern : '23,5',
+		word : 'we'
+	}, {
+		pattern : '19,1,25',
+		word : 'say'
+	}, {
+		pattern : '8,5,18',
+		word : 'her'
+	}, {
+		pattern : '19,8,5',
+		word : 'she'
+	}, {
+		pattern : '15,18',
+		word : 'or'
+	}, {
+		pattern : '1,14',
+		word : 'an'
+	}, {
+		pattern : '23,9,12',
+		word : 'will'
+	}, {
+		pattern : '13,25',
+		word : 'my'
+	}, {
+		pattern : '15,14,5',
+		word : 'one'
+	}, {
+		pattern : '1,12',
+		word : 'all'
+	}, {
+		pattern : '23,15,21,12,4',
+		word : 'would'
+	}, {
+		pattern : '20,8,5,18,5',
+		word : 'there'
+	}, {
+		pattern : '20,8,5,9,18',
+		word : 'their'
+	}, {
+		pattern : '23,8,1,20',
+		word : 'what'
+	}, {
+		pattern : '19,15',
+		word : 'so'
+	}, {
+		pattern : '21,16',
+		word : 'up'
+	}, {
+		pattern : '19,15',
+		word : 'so'
+	}, {
+		pattern : '15,21,20',
+		word : 'out'
+	}, {
+		pattern : '9,6',
+		word : 'if'
+	}, {
+		pattern : '1,2,15,21,20',
+		word : 'about'
+	}, {
+		pattern : '23,8,15',
+		word : 'who'
+	}, {
+		pattern : '7,5,20',
+		word : 'get'
+	}, {
+		pattern : '23,8,9,3,8',
+		word : 'which'
+	}];
 	var tizenDB = {};
 	request = indexedDB.open(baseName);
 	request.onsuccess = function(evt) {
@@ -50,7 +194,7 @@ function showDataRecord(pattern) {
 				console.log(event);
 				var res = event.target.result;
 				console.log(res);
-				document.getElementById("result").innerHTML += res.word + " ";
+				document.getElementById("inputbox").innerHTML += res.word + " ";
 			};
 		} else {
 			console.log("request is null");
--- SpinKeyboard/js/rotaryEvent.js
+++ SpinKeyboard/js/rotaryEvent.js
@@ -1,6 +1,6 @@
 /*global tau */
 (function() {
-	var page = document.getElementById("pageRotaryEvent"), rotaryDetentHandler, currentDirection = "", command = "", counter = 0, value = 0, funcTimeout;
+	var page = document.getElementById("pageRotaryEvent"), rotaryDetentHandler, currentDirection = "", command = [], counter = 0, value = 1, funcTimeout1, funcTimeout2;
 	/**
 	 * pagebeforeshow event handler Do preparatory works and adds event
 	 * listeners
@@ -11,8 +11,10 @@
 		// "rotarydetent" event handler
 		rotaryDetentHandler = function(e) {
 			// Get rotary direction
-			if (funcTimeout != undefined)
-				clearTimeout(funcTimeout);
+			if (funcTimeout1 != undefined)
+				clearTimeout(funcTimeout1);
+			if (funcTimeout2 != undefined)
+				clearTimeout(funcTimeout2);
 			direction = e.detail.direction;
 
 			if (direction === "CW") {
@@ -21,31 +23,31 @@
 				if (currentDirection == "")
 					currentDirection = "CW";
 				if (currentDirection != "CW") {
-					command = command + "-" + counter
-					counter = 0;
 					currentDirection = "CW";
+					command.push(value);
 				}
-				counter++;
 				value++;
 			} else if (direction === "CCW") {
 
 				if (currentDirection == "")
 					currentDirection = "CCW";
 				if (currentDirection != "CCW") {
-					command = command + "+" + counter;
-					counter = 0;
 					currentDirection = "CCW";
+					command.push(value);
 				}
-				counter++;
 				value--;
 			}
-			if (value == 26)
-				value = 0;
-			if (value == -1) {
+			if (value == 27)
+				value = 1;
+			if (value == 0) {
 				value = 26;
 			}
-			$("#cursor").rotate(value * 360 / 26);
-			funcTimeout = setTimeout(finishTyping, 1000);
+			$("#cursor").rotate((value-1) * 360 / 26);
+			function addLetterOnPause() {
+				command.push(value);
+				funcTimeout2 = setTimeout(finishTyping, 1000);
+			}
+			funcTimeout1 = setTimeout(addLetterOnPause, 1000);
 		};
 
 		// Add rotarydetent handler to document
@@ -58,38 +60,29 @@
 	page.addEventListener("pagehide", function() {
 		document.removeEventListener("rotarydetent", rotaryDetentHandler);
 	});
-//	var enterText = document.getElementById("enter_text");
-//	enterText.addEventListener("click", function() {
-//		if (currentDirection == "CW") {
-//			command = command + "+" + counter;
-//		} else {
-//			command = command + "-" + counter;
-//		}
-//		counter = 0;
-//		value = 0;
-//		alert(command);
-//
-//		showDataRecord(command);
-//
-//		command = "";
-//		currentDirection = "";
-//	});
-//
-//	function valueToLetter(value) {
-//		String.fromCharCode(94 + value);
-//	}
+	// var enterText = document.getElementById("enter_text");
+	// enterText.addEventListener("click", function() {
+	// if (currentDirection == "CW") {
+	// command = command + "+" + counter;
+	// } else {
+	// command = command + "-" + counter;
+	// }
+	// counter = 0;
+	// value = 0;
+	// alert(command);
+	//
+	// showDataRecord(command);
+	//
+	// command = "";
+	// currentDirection = "";
+	// });
+	//
+	// function valueToLetter(value) {
+	// String.fromCharCode(94 + value);
+	// }
 	function finishTyping() {
-		if (currentDirection == "CW") {
-			command = command + "+" + counter;
-		} else {
-			command = command + "-" + counter;
-		}
-		counter = 0;
-		value = 0;
-
 		showDataRecord(command);
-
-		command = "";
+		command = [];
 		currentDirection = "";
 	}
 
